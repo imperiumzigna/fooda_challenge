f7925657f70d2acb1961544d19280eabfcd9b95c
Igor Amorim Silva <igoramorimsilva2@gmail.com>
2021-07-09 07:09:20 -0300

    added customer structure

---
+++ services/customer.rb
@@ -0,0 +1,24 @@
+class Customer
+  attr_accessor :name, :orders, :points, :avg_points
+
+  def initialize(name)
+    @name = name
+    @orders = 0
+    @points = 0
+    @avg_points = 0
+  end
+
+  def get_report
+    if orders.zero?
+      "#{name}: No orders."
+    else
+      "#{name}: #{points} points with #{avg_points} points per order."
+    end
+  end
+
+  def update(new_points)
+    self.orders += 1
+    self.points += new_points
+    self.avg_points = points / orders if orders > 0
+  end
+end
---
+++ spec/services/customer_spec.rb
@@ -0,0 +1,5 @@
+require_relative '../spec_helper'
+require_relative '../../services/customer'
+
+describe Customer do
+end
