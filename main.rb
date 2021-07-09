9e90f8a7be5927d16f1f96546c1a964f721e8900
Igor Amorim Silva <igoramorimsilva2@gmail.com>
2021-07-09 07:18:34 -0300

    removed main.rb unused require

--- main.rb
+++ main.rb
@@ -1,10 +1,9 @@
-require 'file'
 require 'json'
 require_relative './services/event_parser'
 
 file = File.read('./data/example.json')
 
-json_payload = JSON.parse(file)
+json = JSON.parse(file)
 
 parser = EventParser.new(json)
 
